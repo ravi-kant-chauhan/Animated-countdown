0469843224232fd0cae8c0309538b5667f9295a0
Ravi <ravkchauhan1aip@gmail.com>
2023-02-05 23:09:31 +0530

    update

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ runAnimation()
 function resetDOM() {
   counter.classList.remove('hide')
   finalMessage.classList.remove('show')
+
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
+
+  nums[0].classList.add('in')
+}
