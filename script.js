fa4599ccada646aed02333b2898197894fbd2b8b
Ravi <ravkchauhan1aip@gmail.com>
2023-02-05 23:09:13 +0530

    update

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+
+runAnimation()
+
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
